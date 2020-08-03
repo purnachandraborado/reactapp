e4cda337d061c07f5eab20e379332be171c8d65e
Purnachandra Borado <purnachandraborado@gmail.com>
2020-08-03 17:19:10 +0530

    Updates

---
+++ precache-manifest.d4853ccbf9b16ae61532ba5b6ad7b95a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5184b8aa20288451e7f256a9663215f",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "283558aa7cf6f25ff162",
+    "url": "/reactapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "99183997a7040693508e",
+    "url": "/reactapp/static/css/main.2e5f40ac.chunk.css"
+  },
+  {
+    "revision": "283558aa7cf6f25ff162",
+    "url": "/reactapp/static/js/2.9616f279.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactapp/static/js/2.9616f279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99183997a7040693508e",
+    "url": "/reactapp/static/js/main.d5ea498a.chunk.js"
+  },
+  {
+    "revision": "d7c21f3c8a0dcd2e6d3a",
+    "url": "/reactapp/static/js/runtime-main.f466f4a8.js"
+  },
+  {
+    "revision": "e71af4a4da184a8b254efaea0bf1c48a",
+    "url": "/reactapp/static/media/3.e71af4a4.jpg"
+  },
+  {
+    "revision": "dc63627b70067cf0a4831665fecd1774",
+    "url": "/reactapp/static/media/bg.dc63627b.jpg"
+  }
+]);
